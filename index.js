24f8411972ce81fb7ee7d3786a77db3979984b94
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-06 20:52:52 -0300

    pesquisa

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ async function main(){
     // Login e Senha
 
     const input =  await page.$('[name ="loginfmt"]')
-    await input.type(process.env)
+    await input.type(process.env.EMAIL)
     await input.press('Enter');
 
     const pass =  await page.$('[name ="passwd"]')
-    await pass.type(process.env)
+    await pass.type(process.env.KEY)
     await page.click('#idSIButton9') //clica no botão de logar da senha, pq o Enter n funciona
     await page.click('#idSIButton9') // clica no botão para continuar que possui o mesmo id
 
@@ -33,29 +33,21 @@ async function main(){
       await page.click("#bnp_btn_accept"); // aceita o modal de pedido de cookies
 
 
-    //   Ganhe até 90 pontos por dia, 3 pontos por pesquisa no COMPUTADOR, 90 / 3 = 30, ent tem que efetuar 30 pesquisas
+    //Pesquisa
+    
+    //Ganhe até 90 pontos por dia, 3 pontos por pesquisa no COMPUTADOR, 90 / 3 = 30, ent tem que efetuar 30 pesquisas
+
+    const Term1 = '1';
+    const SInput = await page.$('[name = "q"]');
+    await SInput.type(Term1)
+    await SInput.press('Enter')
+    
 
 }
 
 main()
 
-
-// async function main(){
-//     const browser = await playwright[browserType].launch({ headless: false });
-//     const context = await browser.newContext();
-//     const page = await context.newPage();
-
-//     await page.goto('https:/google.com') 
-    
-
 //     const searchTerm = 'eu amo automação'; // texto a ser inserido
 //     const input =  await page.$('[name ="q"]') // pegando o input pelo name
 //     await input.type(searchTerm);
 //     await input.press('Enter');
-
-
-//     await page.waitForTimeout(2000);
-//     await page.screenshot({path: 'cap.png'})
-//     await browser.close()
-
-// }
