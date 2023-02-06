4a73a2e18f069eb738a3c07edbd6d28df6d5a454
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-06 20:41:56 -0300

    keep

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const playwright = require('playwright')
+require('dotenv').config();
 
 const browserType = 'chromium';
 
@@ -7,14 +8,54 @@ async function main(){
     const context = await browser.newContext();
     const page = await context.newPage();
 
-    await page.goto('https://google.com')
-    // console.log("oi")
+    await page.goto('https://login.live.com') // Abre a pagina de login
 
-    const searchTerm = 'eu amo automação'; // texto a ser inserido
-    const input =  await page.$('[name ="q"]') // pegando o input pelo name
-    await input.type(searchTerm);
+    // Login e Senha
+
+    const input =  await page.$('[name ="loginfmt"]')
+    await input.type(process.env)
     await input.press('Enter');
+
+    const pass =  await page.$('[name ="passwd"]')
+    await pass.type(process.env)
+    await page.click('#idSIButton9') //clica no botão de logar da senha, pq o Enter n funciona
+    await page.click('#idSIButton9') // clica no botão para continuar que possui o mesmo id
+
+
+    //Abrir Bing
+
+    await page.goto('https://www.bing.com/search?q=awdawd&form=QBLH&sp=-1&pq=&sc=8-0&qs=n&sk=&cvid=C642DE3A760C43F497A0F0B3187CA68F&ghsh=0&ghacc=0&ghpl=')
+
+    setTimeout(async () => {
+        await page.reload();
+      }, 3000); // atualiza a pagina para atualizar os pontos
+
+      await page.click("#bnp_btn_accept"); // aceita o modal de pedido de cookies
+
+
+    //   Ganhe até 90 pontos por dia, 3 pontos por pesquisa no COMPUTADOR, 90 / 3 = 30, ent tem que efetuar 30 pesquisas
 
 }
 
 main()
+
+
+// async function main(){
+//     const browser = await playwright[browserType].launch({ headless: false });
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+
+//     await page.goto('https:/google.com') 
+    
+
+//     const searchTerm = 'eu amo automação'; // texto a ser inserido
+//     const input =  await page.$('[name ="q"]') // pegando o input pelo name
+//     await input.type(searchTerm);
+//     await input.press('Enter');
+
+
+//     await page.waitForTimeout(2000);
+//     await page.screenshot({path: 'cap.png'})
+//     await browser.close()
+
+// }
