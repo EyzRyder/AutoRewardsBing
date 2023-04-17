1680a394fc1668a8b07a708b8188b0dc0c782dd5
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-04-17 06:47:32 -0300

    mobile click start

--- index.js
+++ index.js
@@ -3,93 +3,188 @@ require('dotenv').config();
 
 const browserType = 'chromium';
 
-async function main(){
+// async function main(){
   
-  // Ganhe até 90 pontos por dia, 3 pontos por pesquisa no COMPUTADOR, 90 / 3 = 30. Então o script tem que logar, abrir o bing e efetuar 30 pesquisas
+//   // Ganhe até 90 pontos por dia, 3 pontos por pesquisa no COMPUTADOR, 90 / 3 = 30. Então o script tem que logar, abrir o bing e efetuar 30 pesquisas
   
-    const browser = await playwright[browserType].launch({
-      executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', //abre o edge
-      headless: false,
-     });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+//     const browser = await playwright[browserType].launch({
+//       executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', //abre o edge
+//       headless: false,
+//      });
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
 
-    await page.goto('https://login.live.com') // Abre a pagina de login da microsoft 
+//     await page.goto('https://login.live.com') // Abre a pagina de login da microsoft 
 
-    //Login e Senha
+//     //Login e Senha
 
-    const input =  await page.$('[name ="loginfmt"]')
-    await input.type(process.env.EMAIL)
-    await input.press('Enter');
+//     const input =  await page.$('[name ="loginfmt"]')
+//     await input.type(process.env.EMAIL)
+//     await input.press('Enter');
 
-    const pass =  await page.$('[name ="passwd"]')
-    await pass.type(process.env.KEY)
-    await page.click('#idSIButton9') // clica no botão de logar da senha, pq o Enter n funciona
-    await page.click('#idSIButton9') // clica no botão para continuar que possui o mesmo id
+//     const pass =  await page.$('[name ="passwd"]')
+//     await pass.type(process.env.KEY)
+//     await page.click('#idSIButton9') // clica no botão de logar da senha, pq o Enter n funciona
+//     await page.click('#idSIButton9') // clica no botão para continuar que possui o mesmo id
 
 
-    //Abrir Bing
+//     //Abrir Bing
 
-    await page.goto('https://www.bing.com/search?q=0&qs=n&form=QBRE&sp=-1&pq=&sc=10-0&sk=&cvid=AE59F13D55AE4443A173E4DA65169A6E&ghsh=0&ghacc=0&ghpl=')
+//     await page.goto('https://www.bing.com/search?q=0&qs=n&form=QBRE&sp=-1&pq=&sc=10-0&sk=&cvid=AE59F13D55AE4443A173E4DA65169A6E&ghsh=0&ghacc=0&ghpl=')
 
-    //Pesquisa
+//     //Pesquisa
     
-    const Term1 = '0';
-    const SInput = await page.$('[name = "q"]');
-    await SInput.type(Term1)
-    await SInput.press('Enter')
+//     const Term1 = '0';
+//     const SInput = await page.$('[name = "q"]');
+//     await SInput.type(Term1)
+//     await SInput.press('Enter')
 
-    // Loop de Pesquisa
-    setTimeout(async () => {
+//     // Loop de Pesquisa
+//     setTimeout(async () => {
 
-    // oque cada operação faz esta comentando apartir da linha 71
+//     // oque cada operação faz esta comentando apartir da linha 71
 
-      await page.click("#bnp_btn_accept");
+//       await page.click("#bnp_btn_accept");
 
-        for (let i = 1; i < 2; i++) {
+//         for (let i = 1; i < 1; i++) {
 
-          await page.click('#sb_form_q');
-          await page.click('#sw_clx');
+//           await page.click('#sb_form_q');
+//           await page.click('#sw_clx');
       
-          await page.click('#sb_form_q');
-          const Input = await page.$('#sb_form_q');
-          await Input.type(`Pesquisa Numero ${i}`);
-          await Input.press('Enter');
+//           await page.click('#sb_form_q');
+//           const Input = await page.$('#sb_form_q');
+//           await Input.type(`Pesquisa Numero ${i}`);
+//           await Input.press('Enter');
       
-          await new Promise(resolve => setTimeout(resolve, 500));
-        }
+//           await new Promise(resolve => setTimeout(resolve, 500));
+//         }
 
-        await page.click('#sb_form_q');
-        await page.click('#sw_clx');
+//         await page.click('#sb_form_q');
+//         await page.click('#sw_clx');
 
-        await page.click('#sb_form_q');
-        const Input = await page.$('#sb_form_q');
-        await Input.type(`Por Hoje é Só 👏👏👏`);
+//         await page.click('#sb_form_q');
+//         const Input = await page.$('#sb_form_q');
+//         await Input.type(`Por Hoje é Só 👏👏👏`);
 
-        // await page.waitForTimeout(20000)
-        // await browser.close();
+//         // await page.waitForTimeout(20000)
+//         // await browser.close();
 
-        //Abre a versão mobile para pegar os pontos mobile
+//         //Abre a versão mobile para pegar os pontos mobile
+        
+        
+//       }, 5000);
+      
+//       setTimeout(async () => {
+//         const pageMobile = await context.newPage();
+//         await pageMobile.setViewportSize({ width: 375, height: 812 });
+      
+//         const pages = await context.pages();
+//         await pages[0].close();
+      
+//         await pageMobile.goto('https://www.bing.com/search?q=0&qs=n&form=QBRE&sp=-1&pq=&sc=10-0&sk=&cvid=AE59F13D55AE4443A173E4DA65169A6E&ghsh=0&ghacc=0&ghpl=');
+      
+//         await pageMobile.click('.b_searchbox');
+//         await pageMobile.click('.sw_close');
+      
+//       }, 1000);
+      
+      
+//     }
+
+    async function main(){
+  
+      // Ganhe até 90 pontos por dia, 3 pontos por pesquisa no COMPUTADOR, 90 / 3 = 30. Então o script tem que logar, abrir o bing e efetuar 30 pesquisas
+      
+        const browser = await playwright[browserType].launch({
+          executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', //abre o edge
+          headless: false,
+         });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+    
+        await page.goto('https://login.live.com') // Abre a pagina de login da microsoft 
+    
+        //Login e Senha
+    
+        const input =  await page.$('[name ="loginfmt"]')
+        await input.type(process.env.EMAIL)
+        await input.press('Enter');
+    
+        const pass =  await page.$('[name ="passwd"]')
+        await pass.type(process.env.KEY)
+        await page.click('#idSIButton9') // clica no botão de logar da senha, pq o Enter n funciona
+        await page.click('#idSIButton9') // clica no botão para continuar que possui o mesmo id
+    
+    
+        //Abrir Bing
+    
+        await page.goto('https://www.bing.com/search?q=0&qs=n&form=QBRE&sp=-1&pq=&sc=10-0&sk=&cvid=AE59F13D55AE4443A173E4DA65169A6E&ghsh=0&ghacc=0&ghpl=')
+    
+        //Pesquisa
+        
+        const Term1 = '0';
+        const SInput = await page.$('[name = "q"]');
+        await SInput.type(Term1)
+        await SInput.press('Enter')
+    
+        // Loop de Pesquisa
         setTimeout(async () => {
-          await context.newPage();
-          const pages = await context.pages();
-          const pageMobile = pages[1];
-          await pageMobile.setViewportSize({ width: 375, height: 812 });
-        
-          //fecha a aba normal e abre a mobile
-          const page = pages[0];
-          await page.close();
-        
-          await pageMobile.goto('https://www.bing.com/search?q=0&qs=n&form=QBRE&sp=-1&pq=&sc=10-0&sk=&cvid=AE59F13D55AE4443A173E4DA65169A6E&ghsh=0&ghacc=0&ghpl=');
-        
+    
+        // oque cada operação faz esta comentando apartir da linha 71
+    
+          await page.click("#bnp_btn_accept");
+    
+            for (let i = 1; i < 1; i++) {
+    
+              await page.click('#sb_form_q');
+              await page.click('#sw_clx');
+          
+              await page.click('#sb_form_q');
+              const Input = await page.$('#sb_form_q');
+              await Input.type(`Pesquisa Numero ${i}`);
+              await Input.press('Enter');
+          
+              await new Promise(resolve => setTimeout(resolve, 500));
+            }
+    
+            await page.click('#sb_form_q');
+            await page.click('#sw_clx');
+    
+            await page.click('#sb_form_q');
+            const Input = await page.$('#sb_form_q');
+            await Input.type(`Por Hoje é Só 👏👏👏`);
+    
+            // await page.waitForTimeout(20000)
+            // await browser.close();
+    
+            
+          }, 5000);
           
 
-        
+        //Loop Mobile  
+        setTimeout(async () => {
+          const pageMobile = await context.newPage();
+          await pageMobile.setViewportSize({ width: 375, height: 812 });
+          
+          const pages = await context.pages();
+          await pages[0].close();
+          
+          await pageMobile.goto('https://www.bing.com/search?q=0&qs=n&form=QBRE&sp=-1&pq=&sc=10-0&sk=&cvid=AE59F13D55AE4443A173E4DA65169A6E&ghsh=0&ghacc=0&ghpl=');
+          
+          await pageMobile.click('#sb_form_q');
+          await pageMobile.click('#sw_clx');
+          
+          await pageMobile.click('#sb_form_q');
+          const Input = await pageMobile.$('#sb_form_q');
+          await Input.type(`Pesquisa Numero a`);
+          await pageMobile.click('.b_searchboxSubmit')
+
+          // bnp_btn_accept botão do cookie
+          
         }, 1000);
         
-
-      }, 5000);
-
-    }
+          
+          
+        }
 
 main()
